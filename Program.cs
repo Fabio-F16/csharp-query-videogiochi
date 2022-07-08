c9d0c72ec34c898c858b66c5f0f9c1d03d6bbeee
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-08 14:22:59 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_query_videogiochi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
